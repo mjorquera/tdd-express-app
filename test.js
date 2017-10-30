7e0d3cd454b57ea08cf26bfaf267c3d03d62e1b2
Mario Jorquera <mariojz@gmail.com>
2017-10-30 14:55:32 -0300

    Add validations

--- test.js
+++ test.js
@@ -62,6 +62,13 @@ describe('Creating new cities', function(){
             .send('name=Springfield&description=where+the+Simpson+lives')
             .expect(/springfield/i,done);
     });
+
+    it('Validates city name and description', function(done){
+        request(app)
+            .post('/cities')
+            .send('name=&description=')
+            .expect(400,done);
+    });
 });
 
 describe('Deleting cities', function(){
