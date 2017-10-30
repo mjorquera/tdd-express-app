a68d64924ef5d2abab53b040b4445fd222a960c7
Mario Jorquera <mariojz@gmail.com>
2017-10-30 11:20:55 -0300

    Create new cities

--- test.js
+++ test.js
@@ -41,3 +41,19 @@ describe('Listing cities on /cities', function(){
             .expect(JSON.stringify(['Lotopia','Caspiana','Indigo']), done);
     });
 });
+
+describe('Creating new cities', function(){
+    it('Returns a 201 status code', function(done){
+        request(app)
+            .post('/cities')
+            .send('name=Springfield&description=where+the+Simpson+lives')
+            .expect(201,done);
+    });
+
+    it('Return the city name', function(done){
+        request(app)
+            .post('/cities')
+            .send('name=Springfield&description=where+the+Simpson+lives')
+            .expect(/springfield/i,done);
+    });
+});
