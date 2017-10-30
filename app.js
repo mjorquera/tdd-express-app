373370b103c0a78c3a4b39f69b1b0e301943bac2
Mario Jorquera <mariojz@gmail.com>
2017-10-30 14:30:10 -0300

    No select for production

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ if (process.env.REDISTOGO_URL) {
     client.auth(rtg.auth.split(":")[1]);
 } else {
     var client = redis.createClient();
+    client.select((process.env.NODE_ENV || 'development').length);
 }
-
-
-client.select((process.env.NODE_ENV || 'development').length);
 
 app.get('/cities', function(request, response){
     client.hkeys('cities', function(error, names){
